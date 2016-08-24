53123ed5532e96d5c7073c165560d6dc04063a32
Logan <lsaruwatari@gmail.com>
2016-08-24 13:20:48 -0600

    Create set.js

---
+++ set.js
@@ -0,0 +1,13 @@
+class card{
+  
+  constructor(color,shape,fill,count){
+    this.color = color;
+    this.shape = shape;
+    this.fill = fill;
+    this.count = count;
+  }
+}
+
+class deck{
+
+}
